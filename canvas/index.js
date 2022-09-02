6a522d9e01ea4393ad97468e0ea50ba616eed05a
satyaperumalla <satyaperumalla09@gmail.com>
2022-09-02 23:31:16 +0530

    canvas project

---
+++ canvas/index.js
@@ -0,0 +1,51 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d'); //Returns an object that provides methods and properties for drawing.
+
+canvas.width = window.innerWidth; //interior width of the window in pixels. This includes the width of the vertical scroll bar, if one is present.
+canvas.height = window.innerHeight; //interior height of the window in pixels, including the height of the horizontal scroll bar, if present.
+
+ctx.strokeStyle = '#BADA55'; //Color
+ctx.lineJoin = 'round'; //shape used to join two line segments where they meet.
+ctx.lineCap = 'round';
+ctx.lineWidth = 0;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+
+function draw(e){
+    if(!isDrawing) return;
+    ctx.beginPath();
+    ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    [lastX,lastY] = [e.offsetX,e.offsetY];
+    hue++
+    if(hue >= 360){
+        hue = 0;
+    }
+    if(ctx.lineWidth >=100 || ctx.lineWidth <= 1){
+        direction = !direction;
+    }
+    if(direction){
+        ctx.lineWidth++;
+    }else{
+        ctx.lineWidth--;
+    }
+
+    console.log(ctx.lineWidth);
+}
+
+
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mousedown',(e)=>{
+    isDrawing=true;
+    [lastX,lastY] = [e.offsetX,e.offsetY]
+});
+canvas.addEventListener('mouseup',()=>isDrawing=false);
+canvas.addEventListener('mouseout',()=>isDrawing=false);
+
